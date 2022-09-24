2ad67bd6c1c1aa30e130f4e299e54e880818c425
jshuaaaa <95507083+jshuaaaa@users.noreply.github.com>
2022-09-24 12:54:59 -0400

    setup backtesting as well as cumulative return testing

--- strategy_backtesting.py
+++ strategy_backtesting.py
@@ -1,6 +1,8 @@
 import yfinance as yf
 import numpy as np
 import copy
+import pandas as pd
+import matplotlib.pyplot as plt
 
 stocks = ["JPY=X", "EUR=X"]
 clhv = {}
@@ -36,17 +38,22 @@ def RSI(DF, n=14):
     df["rsi"] = 100 - (100/(1+df["rs"]))
     return df["rsi"]
 
+
+
 # Downloading Data
 tickers_signal = {}
-tickers_ret = {} 
+tickers_ret = {}
 for ticker in stocks:
-    temp = yf.download(ticker, period='1mo', interval='15m')
+    temp = yf.download(ticker, period='60d', interval='15m')
     temp.dropna(how="any", inplace=True)
     clhv[ticker] = temp
     clhv[ticker][['tenkan_sen', 'kijun_sen','senkou_span_a', 'senkou_span_b', 'chikou_span']] = ichimoku_cloud(clhv[ticker])
     clhv[ticker]["RSI"] = RSI(clhv[ticker])
     tickers_signal[ticker] = ""
     tickers_ret[ticker] = []
+    
+
+        
     
     
 ############################ BACKTESTING ###############################
@@ -66,11 +73,44 @@ for ticker in stocks:
     df[ticker]['price_tenkan_cross'] = np.NaN
     df[ticker]['price_tenkan_cross'] = np.where((df[ticker]['Open'].shift(1) <= df[ticker]['tenkan_sen'].shift(1)) & (df[ticker]['Open'] > df[ticker]['tenkan_sen']), 1, df[ticker]['price_tenkan_cross'])
     df[ticker]['price_tenkan_cross'] = np.where((df[ticker]['Open'].shift(1) >= df[ticker]['tenkan_sen'].shift(1)) & (df[ticker]['Open'] < df[ticker]['tenkan_sen']), -1, df[ticker]['price_tenkan_cross'])
+
+
+strategy_df = pd.DataFrame()
+ 
+for ticker in stocks:
+    print("calculating returns for ", ticker)
+
+    for i in range(len(df[ticker])):
+        if tickers_signal[ticker] == "":
+            tickers_ret[ticker].append(0)
+            if (((df[ticker]["above_cloud"][i-1] == 1)  and (df[ticker]["A_above_B"][i-1] == 1)  and (df[ticker]['tenkan_kiju_cross'][i-1]==1))  or df[ticker]['price_tenkan_cross'][i-1] == 1)  and df[ticker]["RSI"][i] > 60:
+                tickers_signal[ticker] = "Buy"
+            elif (((df[ticker]["above_cloud"][i-1] == -1)  and (df[ticker]["A_above_B"][i-1] == -1)  and (df[ticker]['tenkan_kiju_cross'][i-1]==-1))  or df[ticker]['price_tenkan_cross'][i-1] == -1)  and df[ticker]["RSI"][i] < 40:
+                tickers_signal[ticker] = "Sell"
+                
+                
+        elif tickers_signal[ticker] == "Buy":
+            tickers_ret[ticker].append(((df[ticker]["Adj Close"][i-1]/df[ticker]["Adj Close"][i])-1))
+            if (((df[ticker]["above_cloud"][i-1] == -1)  and (df[ticker]["A_above_B"][i-1] == -1)  and (df[ticker]['tenkan_kiju_cross'][i-1]==-1))  or df[ticker]['price_tenkan_cross'][i-1] == -1)  and df[ticker]["RSI"][i] < 40:
+                tickers_signal[ticker] = "Sell"
+         
+            
+        elif tickers_signal[ticker] == "Sell":
+            tickers_ret[ticker].append(((df[ticker]["Adj Close"][i]/df[ticker]["Adj Close"][i-1])-1))
+            if (((df[ticker]["above_cloud"][i-1] == 1)  and (df[ticker]["A_above_B"][i-1] == 1)  and (df[ticker]['tenkan_kiju_cross'][i-1]==1))  or df[ticker]['price_tenkan_cross'][i-1] == 1)  and df[ticker]["RSI"][i] > 60:
+                tickers_signal[ticker] = "Buy"
     
+    df[ticker]["ret"] = np.array(tickers_ret[ticker])
+
     
-    
-    
-    
+
+
+strategy_df = pd.DataFrame()
+for ticker in stocks:
+    strategy_df[ticker] = df[ticker]["ret"]
+strategy_df["ret"] = strategy_df.mean(axis=1)  
+
+(1+strategy_df["ret"]).cumprod().plot() 
     
     
     
