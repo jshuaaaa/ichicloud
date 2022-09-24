67512d12a869383074f59362d5c88995969fd94e
jshuaaaa <95507083+jshuaaaa@users.noreply.github.com>
2022-09-24 09:56:44 -0400

    making ichimoku cloud indicator, added tenkan sen

--- ichimoku_cloud.py
+++ ichimoku_cloud.py
@@ -10,3 +10,10 @@ for ticker in stocks:
     temp = yf.download(ticker, period='1mo', interval='15m')
     temp.dropna(how="any", inplace=True)
     clhv[ticker] = temp
+
+
+def ichimoku_cloud(DF):
+    df = DF.copy()
+    nine_period_high = df['High'].rolling(window= 9).max()
+    nine_period_low = df['Low'].rolling(window= 9).min()
+    df['tenkan_sen'] = (nine_period_high + nine_period_low) /2
