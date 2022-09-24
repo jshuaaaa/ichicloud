fe299cbee5dafe8bbd29a5d909df47da8e05ff1f
jshuaaaa <95507083+jshuaaaa@users.noreply.github.com>
2022-09-24 10:09:21 -0400

    setup architecture for backtesting strategy

---
+++ strategy_backtesting.py
@@ -0,0 +1,72 @@
+import yfinance as yf
+import numpy as np
+import copy
+
+stocks = ["JPY=X", "EUR=X"]
+clhv = {}
+
+# Indicators  
+def ichimoku_cloud(DF):
+    df = DF.copy()
+    nine_period_high = df['High'].rolling(window= 9).max()
+    nine_period_low = df['Low'].rolling(window= 9).min()
+    df['tenkan_sen'] = (nine_period_high + nine_period_low) /2
+    
+    period26_high = df['High'].rolling(window=26).max()
+    period26_low = df['Low'].rolling(window=26).min()
+    df['kijun_sen'] = (period26_high + period26_low) / 2
+    
+    df['senkou_span_a'] = ((df['tenkan_sen'] + df['kijun_sen']) / 2).shift(26)
+    # Senkou Span B (Leading Span B): (52-period high + 52-period low)/2))
+    period52_high = df['High'].rolling(window=52).max()
+    period52_low = df['Low'].rolling(window=52).min()
+    df['senkou_span_b'] = ((period52_high + period52_low) / 2).shift(26)
+    # The most current closing price plotted 26 time periods behind (optional)
+    df['chikou_span'] = df['Close'].shift(-26)
+    return df.loc[:,['tenkan_sen', 'kijun_sen','senkou_span_a', 'senkou_span_b', 'chikou_span']]
+
+def RSI(DF, n=14):
+    df = DF.copy()
+    df["change"] = df["Adj Close"] - df["Adj Close"].shift(1)
+    df["gain"] = np.where(df["change"]>=0,df["change"], 0)
+    df["loss"] = np.where(df["change"]<0,-1*df["change"], 0)
+    df["avgGain"] = df["gain"].ewm(alpha=1/n, min_periods=n).mean()
+    df["avgLoss"] = df["loss"].ewm(alpha=1/n, min_periods=n).mean()
+    df["rs"] = df["avgGain"]/df["avgLoss"]
+    df["rsi"] = 100 - (100/(1+df["rs"]))
+    return df["rsi"]
+
+# Downloading Data
+tickers_signal = {}
+tickers_ret = {} 
+for ticker in stocks:
+    temp = yf.download(ticker, period='1mo', interval='15m')
+    temp.dropna(how="any", inplace=True)
+    clhv[ticker] = temp
+    clhv[ticker][['tenkan_sen', 'kijun_sen','senkou_span_a', 'senkou_span_b', 'chikou_span']] = ichimoku_cloud(clhv[ticker])
+    tickers_signal[ticker] = ""
+    tickers_ret[ticker] = []
+    
+    
+############################ BACKTESTING ###############################
+
+df = copy.deepcopy(clhv)
+ 
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
